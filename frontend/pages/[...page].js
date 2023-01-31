90b3a9baeab0a0f55d1c2afad94ab07368c1bf6a
Oliver Daniel <oliverdaniel00@gmail.com>
2023-01-31 15:13:31 -0500

    catch up frontend improvements

--- frontend/pages/[...page].js
+++ frontend/pages/[...page].js
@@ -1,27 +1,43 @@
 import PageLayout from "../components/layout/page/PageLayout";
 import { Markdown, serialize } from "../lib/mdx-remote";
+import { SEO } from "../lib/seo";
 import PageService from "../services/PageService";
 
 const GenericPage = ({ slug, title, description, content }) => {
-  // TODO head
+  // TODO: this works but do it server-side
+  // const scope = {
+  //   updated_at: new Date(updated_at).toLocaleString(),
+  // }
   return (
-    <PageLayout>
-      <h1>{title}</h1>
-      <Markdown {...content}/>
-    </PageLayout>
+    <>
+      <SEO title={title} description={description} />
+      <PageLayout>
+        <h1>{title}</h1>
+        {content.map(([section_id, text]) => (
+          <section id={section_id} key={section_id}>
+            <Markdown {...text} /*scope={scope}*/ />
+          </section>
+        ))}
+      </PageLayout>
+    </>
   );
 };
 
 export async function getStaticProps({ params }) {
   const { page: tokens } = params;
   const slug = tokens.join("/");
-  const fields = ["slug", "title", "description", "content"];
+  const fields = ["slug", "title", "description", "content", "updated_at"];
 
   const page = await PageService.getPageBySlug(slug, {
     fields,
   });
 
-  page.content = await serialize(page.content);
+  page.content = await Promise.all(
+    page.content.map(async ({ translation_id, text }) => [
+      translation_id,
+      await serialize(text),
+    ])
+  );
 
   return {
     props: {
