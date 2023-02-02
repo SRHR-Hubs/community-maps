b070d0f28e7a0aa1cf6105871d32d489f351c780
Oliver Daniel <oliverdaniel00@gmail.com>
2023-02-02 12:04:05 -0500

    format i18n api to expected shape

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
 import rest_framework.viewsets as vs
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from collections import defaultdict
+from functools import reduce as _reduce
+
 
 from . import models, serializers
 
@@ -21,5 +25,19 @@ class PageViewset(vs.ModelViewSet):
 class I18nSectionViewset(vs.ModelViewSet):
     queryset = models.I18nSection.objects.all()
     serializer_class = serializers.I18nSectionSerializer
-    lookup_field = 'translation_id'
+    lookup_field = 'language'
     filter_fields = '__all__'
+
+    def retrieve(self, request, language=None):
+        def nested_dict(): return defaultdict(nested_dict)
+        response = nested_dict()
+
+        for item in self.get_queryset().filter(language=language):
+            *namespaces, _id = item.translation_id.split('.')
+            # create dict keys recursively to drill down to the namespace
+            curr = response
+            for key in namespaces or ['common']:
+                curr = curr[key]
+            curr[_id] = item.text
+
+        return Response(response)
