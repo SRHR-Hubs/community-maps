b7435688856d0ab953e3ddc2e7393d89d4412a01
Oliver Daniel <oliverdaniel00@gmail.com>
2023-02-09 16:15:29 -0500

    replace ionicons with lucide

--- frontend/components/layout/header/Header.js
+++ frontend/components/layout/header/Header.js
@@ -1,6 +1,7 @@
 import Logo from "../../../public/logo.svg";
 import Link from "next/link";
-import { Twitter, Instagram } from "../../../lib/ionicons";
+import { Twitter, Instagram } from "lucide-react";
+// import Twitter
 import config from "../../../config/next-seo.config";
 import { Trans } from "next-i18next";
 
@@ -9,46 +10,60 @@ const { twitter: twitterInfo, instagram: instagramInfo } = config;
 const Header = ({ show, ...props }) => (
   <header data-show={show} role="banner">
     <div className="container grid-xl">
-      <div className="columns" id="header-top-row">
-        <section className="col-ml-auto links" role="navigation">
-          <Link href="/accessibility">Accessibility</Link>
-          <Link href="/join-our-team">Get involved</Link>
-          <Link href="#"><button className="btn btn-error get-out-quick">Get out quick</button></Link>
-        </section>
-        <div className="col-1 dummy"></div>
-      </div>
-      <div className="columns" id="header-bottom-row">
+      <div className="columns">
         <section className="navbar-section column col-2 hide-md" aria-hidden>
           <Link href="/" tabIndex={-1}>
             <Logo className="logo" />
           </Link>
         </section>
-        <section className="navbar-section column col links">
-          <Link href="/">Home</Link>
-          <Link href="/about">About Us</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/contact">Contact</Link>
-        </section>
-        <section className="navbar-section column col-2 links hide-md">
-          <Link
-            href={twitterInfo.site}
-            className="tooltip tooltip-bottom"
-            data-tooltip={twitterInfo.handle}
-            aria-label="Follow SRHR Hubs on Twitter"
-          >
-            <Twitter />
-          </Link>
-          <Link
-            href={instagramInfo.site}
-            className="tooltip tooltip-bottom"
-            data-tooltip={instagramInfo.handle}
-            aria-label="Follow SRHR Hubs on Instagram"
-          >
-            <Instagram />
-          </Link>
-          <button className="btn btn-primary"><Trans i18nKey={'layout.header.use-the-map'}>Use the map</Trans></button>
-        </section>
-        <div className="col-1 dummy"></div>
+        <div className="column col rows">
+          <div className="columns" id="header-top-row">
+            <section className="col-ml-auto links" role="navigation">
+              <Link href="/accessibility">Accessibility</Link>
+              <Link href="/join-our-team">Get involved</Link>
+              <Link href="#">
+                <button className="btn btn-error get-out-quick">
+                  Get out quick
+                </button>
+              </Link>
+            </section>
+          </div>
+          <div className="columns" id="header-bottom-row">
+            <div className="link-container columns">
+              <section className="navbar-section column col links">
+                <Link href="/">Home</Link>
+                <Link href="/about">About Us</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/contact">Contact</Link>
+              </section>
+              <section className="navbar-section column col-3 links hide-md">
+                <Link
+                  href={twitterInfo.site}
+                  className="tooltip tooltip-bottom"
+                  data-tooltip={twitterInfo.handle}
+                  aria-label="Follow SRHR Hubs on Twitter"
+                >
+                  <Twitter />
+                </Link>
+                <Link
+                  href={instagramInfo.site}
+                  className="tooltip tooltip-bottom"
+                  data-tooltip={instagramInfo.handle}
+                  aria-label="Follow SRHR Hubs on Instagram"
+                >
+                  <Instagram />
+                </Link>
+                <Link href="#">
+                  <button className="btn btn-primary">
+                    <Trans i18nKey={"layout.header.use-the-map"}>
+                      Use the map
+                    </Trans>
+                  </button>
+                </Link>
+              </section>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
--- frontend/components/sections/contact/ContactSection.js
+++ frontend/components/sections/contact/ContactSection.js
@@ -1,9 +1,21 @@
-import { Trans } from 'next-i18next'
+import { Trans } from "next-i18next";
 
 const ContactSection = () => (
-    <section id="contact">
-        <h2><Trans i18nKey="pages.common.sections.contact.title">Let&apos;s get in touch.</Trans></h2>
-    </section>
-)
+  <section id="contact">
+    <h2>
+      <Trans i18nKey="pages.common.sections.contact.title">
+        Let&apos;s get in touch.
+      </Trans>
+    </h2>
+    <p>
+      <Trans i18nKey="pages.common.sections.contact.content">
+        Have any thoughts, comments or suggestions? Know of any services that
+        should be listed on the SRHR Hubs Community Map?
+        <br />
+        Don&apos;t hesitate to reach out. We&apos;d love to hear from you!
+      </Trans>
+    </p>
+  </section>
+);
 
-export default ContactSection
+export default ContactSection;
