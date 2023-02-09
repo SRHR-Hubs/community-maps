c976380b46d734f7fa74a05e9be43fa26cb67c71
Oliver Daniel <oliverdaniel00@gmail.com>
2023-02-09 17:51:38 -0500

    remove blog from sitemaps

--- frontend/config/next-sitemap.config.js
+++ frontend/config/next-sitemap.config.js
@@ -3,4 +3,7 @@ module.exports = {
     siteUrl: process.env.FLY_APP_NAME || 'https://localhost:3000', // TODO
     generateRobotsTxt: true,
     generateIndexSitemap: false,
+    exclude: [
+        '/blog*',
+    ]
 }
