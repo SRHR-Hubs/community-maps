fd7fa112f53a1f0783a49a30f5d12c09e511988b
Oliver Daniel <oliverdaniel00@gmail.com>
2023-02-11 08:56:28 -0500

    add sitemap to footer

--- frontend/components/layout/footer/Footer.js
+++ frontend/components/layout/footer/Footer.js
@@ -51,6 +51,9 @@ const Footer = (props) => {
               <li>
                 <Link href="/terms-of-service">Terms of Service</Link>
               </li>
+              <li>
+                <Link href="/sitemap.xml">Sitemap</Link>
+              </li>
             </ul>
           </div>
           <div className="column col-md-12">&copy; SRHR Hubs {year}.</div>
