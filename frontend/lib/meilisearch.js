0a7dadfc35d37e139b784d433cd395ebbc9d3540
Oliver Daniel <oliverdaniel00@gmail.com>
2023-06-14 01:02:51 -0400

    God please

--- frontend/lib/meilisearch.js
+++ frontend/lib/meilisearch.js
@@ -1,8 +1,6 @@
 import Meilisearch from "meilisearch";
 
-console.log(process.env.NEXT_PUBLIC_MEILISEARCH_HOST, process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY)
-
 export const client = new Meilisearch({
   host: process.env.NEXT_PUBLIC_MEILISEARCH_HOST,
-  apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_KEY,
 });
