390d1d4d84f7d9f8518ba0dae7d0f281a1b900ef
Oliver Daniel <oliverdaniel00@gmail.com>
2023-06-13 22:36:00 -0400

    CI Actions and workflows for split env (#12)
    Refactor all three apps to allow for the production of preview apps in a staging environment.

---
+++ backend/api/settings/development.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+print(f"Loading settings: {__file__}")
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+assert load_dotenv(
+    BASE_DIR / '.env.local',
+    verbose=True,
+    override=False
+)
+
+from .base import *
+
+SECRET_KEY = getenv('INSECURE_LOCAL_KEY')
