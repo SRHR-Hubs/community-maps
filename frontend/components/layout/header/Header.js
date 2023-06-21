0067a67069eebe6a4835153bdf3b27f327e90b49
Oliver Daniel <oliverdaniel00@gmail.com>
2023-06-21 12:58:57 -0400

    scaffolding for mobile header

--- frontend/components/layout/header/Header.js
+++ frontend/components/layout/header/Header.js
@@ -5,62 +5,85 @@ import { Twitter, Instagram } from "lucide-react";
 import config from "../../../config/next-seo.config";
 import { Trans } from "next-i18next";
 import GetOutQuick from "../get-out-quick/GetOutQuick";
+import { useState } from "react";
+import { clsx } from "clsx";
 
 const { twitter: twitterInfo, instagram: instagramInfo } = config;
 
-const Header = ({ show, ...props }) => (
-  <header data-show={show} role="banner">
-    <div className="container grid-lg">
-      <div className="columns">
-        <section className="navbar-section column col-2 hide-md" aria-hidden>
-          <Link href="/" tabIndex={-1}>
-            <Logo className="logo" />
-          </Link>
-        </section>
-        <div className="column col rows">
-          <div className="columns" id="header-top-row">
-            <section className="col-ml-auto links" role="navigation">
-              <GetOutQuick />
-            </section>
-          </div>
-          <div className="columns" id="header-bottom-row">
-            <div className="link-container columns">
-              <section className="navbar-section column col links">
-                <Link href="/">Home</Link>
-                <Link href="/about">About Us</Link>
-                <Link href="#contact">Contact</Link>
+const Header = ({ show, ...props }) => {
+  const [mobileNavOpen, toggleMobileNav] = useState(false);
+
+  const handleOpenNav = (e) => toggleMobileNav(true);
+  const handleCloseNav = (e) => toggleMobileNav(false);
+
+  return (
+    <header data-show={show} role="banner">
+      <div className="container grid-lg hide-lg">
+        <div className="columns">
+          <section className="navbar-section column col-2 hide-md" aria-hidden>
+            <Link href="/" tabIndex={-1}>
+              <Logo className="logo" />
+            </Link>
+          </section>
+          <div className="column col rows">
+            <div className="columns" id="header-top-row">
+              <section className="col-ml-auto links" role="navigation">
+                <GetOutQuick />
               </section>
-              <section className="navbar-section column col-4 col-md-12 links">
-                <Link
-                  href={instagramInfo.site}
-                  className="tooltip tooltip-bottom hide-md"
-                  data-tooltip={instagramInfo.handle}
-                  aria-label="Follow SRHR Hubs on Instagram"
-                >
-                  <Instagram />
-                </Link>
-                <Link
-                  href={twitterInfo.site}
-                  className="tooltip tooltip-bottom hide-md"
-                  data-tooltip={twitterInfo.handle}
-                  aria-label="Follow SRHR Hubs on Twitter"
-                >
-                  <Twitter />
-                </Link>
-                <Link href="/map" prefetch={false}>
-                  <button className="btn btn-primary" tabIndex={-1}>
-                    <Trans i18nKey={"layout.header.use-the-map"}>
-                      Use the map
-                    </Trans>
-                  </button>
-                </Link>
-              </section>
+            </div>
+            <div className="columns" id="header-bottom-row">
+              <div className="link-container columns">
+                <section className="navbar-section column col links">
+                  <Link href="/">Home</Link>
+                  <Link href="/about">About Us</Link>
+                  <Link href="#contact">Contact</Link>
+                </section>
+                <section className="navbar-section column col-4 col-md-12 links">
+                  <Link
+                    href={instagramInfo.site}
+                    className="tooltip tooltip-bottom hide-md"
+                    data-tooltip={instagramInfo.handle}
+                    aria-label="Follow SRHR Hubs on Instagram"
+                  >
+                    <Instagram />
+                  </Link>
+                  <Link
+                    href={twitterInfo.site}
+                    className="tooltip tooltip-bottom hide-md"
+                    data-tooltip={twitterInfo.handle}
+                    aria-label="Follow SRHR Hubs on Twitter"
+                  >
+                    <Twitter />
+                  </Link>
+                  <Link href="/map" prefetch={false}>
+                    <button className="btn btn-primary" tabIndex={-1}>
+                      <Trans i18nKey={"layout.header.use-the-map"}>
+                        Use the map
+                      </Trans>
+                    </button>
+                  </Link>
+                </section>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+      <div className="off-canvas">
+        <Link href="/" tabIndex={-1}>
+          <Logo className="logo show-lg" />
+        </Link>
+        <button className="btn btn-action show-lg" onClick={handleOpenNav}>
+          <i className="icon icon-menu"></i>
+        </button>
+        <nav className={clsx("off-canvas-sidebar", { active: mobileNavOpen })}>
+          <div className="off-canvas-content">
+            
+          </div>
+        </nav>
+        <div className="off-canvas-overlay" onClick={handleCloseNav} />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
